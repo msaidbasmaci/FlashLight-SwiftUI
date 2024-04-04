f35af091fd69527e43b3fb0014ec4ecaf3301d3c
msaidbasmaci <muhammedbsmcm@gmail.com>
2024-04-04 13:20:50 +0300

    Initial Commit

---
+++ FlashLight/FlashLightApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashLightApp.swift
+//  FlashLight
+//
+//  Created by Muhammed Said BASMACI on 4.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct FlashLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
